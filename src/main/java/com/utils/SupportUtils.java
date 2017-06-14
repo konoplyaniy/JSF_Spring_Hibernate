8f9a8f4d8cdc87062797dcbbfc7337ddfe8a4895
geser <s.konoplyaniy@gmail.com>
2017-06-14 21:39:33 +0300

    comment redundant dependencies, comment md5 convert method for password

--- src/main/java/com/utils/SupportUtils.java
+++ src/main/java/com/utils/SupportUtils.java
@@ -1,7 +1,5 @@
 package com.utils;
 
-import java.security.MessageDigest;
-
 import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.stereotype.Component;
 
@@ -9,17 +7,14 @@ import org.springframework.stereotype.Component;
 public class SupportUtils {
 
 	public static String MD5(String md5) {
-		try {
-			java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+
+			/*java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
 			byte[] array = md.digest(md5.getBytes());
 			StringBuffer sb = new StringBuffer();
 			for (int i = 0; i < array.length; ++i) {
 				sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
-			}
-			return sb.toString();
-		} catch (java.security.NoSuchAlgorithmException e) {
-		}
-		return null;
+			}*/
+			return md5;
 	}
 
 	public static String generatePassword() {
